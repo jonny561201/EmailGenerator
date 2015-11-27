66dd1b9ec48070f65e546303ba9076380802695c
Jon Graf <jongraf1@gmail.com>
2015-11-27 13:31:58 -0600

    Initial commit with tests for validating the object returns a customer name and email address.

---
+++ CustomerInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailGenerator
+{
+    class CustomerInfo
+    {
+        private string customerName { get; set;}
+        private string customerEmail { get; set; }
+
+        public CustomerInfo(string name ="", string email = "")
+        {
+            customerName = name;
+            customerEmail = email;
+        }
+
+        public string CustomerName()
+        {
+            return customerName;
+        }
+
+        public string CustomerEmail()
+        {
+            return customerEmail;
+        }
+    }
+}
---
+++ CustomerInfoTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EmailGenerator
+{
+    [TestFixture]
+    class CustomerInfoTest
+    {
+        [Test]
+        public void CustomerInfoContainName()
+        {
+            CustomerInfo customer = new CustomerInfo("Test");
+            Assert.AreEqual("Test",customer.CustomerName());
+        }
+
+        [Test]
+        public void CustomerInfoContainEMail()
+        {
+            CustomerInfo customer = new CustomerInfo(email:"jonny561201@hotmail.com");
+            Assert.AreEqual("jonny561201@hotmail.com", customer.CustomerEmail());
+
+        }
+    }
+}
